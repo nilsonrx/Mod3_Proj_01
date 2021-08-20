770b24ed5a27d7d080e776f98175fc7befaa433f
Nilson Reis Xavier <85651222+nilsonrx@users.noreply.github.com>
2021-08-20 15:23:55 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
+//Importando o express
+
 const express = require('express')
 const app = express();
+
+//Definindo a Porta
 
 const port = 3000;
 const filmes = ['Resident Evil','Missão Impossível','Esquadrão Suicida','Rambo','Final Fantasy',];
 
-//GET Home 
+//GET Home
 app.get('/', (req, res) => {
     res.send("Sejam Bem Vindos A Filmes S/A.");
 });
 
+//Lendo os filmes
 app.get('/filmes', (req, res) =>{
     res.send(filmes);
 });
 
+//GET /Filmes /ID, listando pelo iD
+
 app.get('/filmes/:id', (req, res) =>{
     const id = req.params.id;
     const filme = filmes[id];
+    if(!filme){
+        res.send('Nenhum Filme encontrado')
     res.send(filme);
+    }
 
 });
 
